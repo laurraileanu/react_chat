7441c40eea64f19123d8a32f05b06ba1964a9f72
Laur <laur.gabriel45@gmail.com>
2020-07-23 18:54:25 +0300

    chat list component

--- src/components/Dashboard/ChatList/index.js
+++ src/components/Dashboard/ChatList/index.js
@@ -1,10 +1,91 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import NotificationImportant from '@material-ui/icons/NotificationImportant';
 
 const ChatList = (props) => {
+  const {classes} = props
+
+  const newChat = () => {
+    console.log('new chat click')
+  }
+
+  const selectChat = (index) => {
+    console.log('select chat', index)
+  }
+
+  if(props.chats.length === 0) {
+    return(
+      <main className={classes.root}>
+        <Button 
+        variant="contained" 
+        fullWidth
+        color="primary" 
+        className={classes.newChatBtn}
+        onClick={newChat} 
+        >
+          New chat
+        </Button>
+        No chat available
+      </main>
+    )
+  }
+  
   return (
-    <div>Chat list</div>
+    <main className={classes.root}>
+      <Button 
+        variant="contained" 
+        fullWidth
+        color="primary" 
+        className={classes.newChatBtn}
+        onClick={newChat} 
+        >
+          New chat
+        </Button>
+      <List>
+        {
+          props.chats.map((chat, index) => {
+            const username = chat.users.filter(user => user !== props.userEmail)[0]
+            
+            return(
+              <div key={index}>
+                <ListItem 
+                  onClick={() => selectChat(index)}
+                  className={classes.listItem}
+                  selected={props.selectChatIndex === index}
+                  alignItems='flex-start'
+                >
+                  <ListItemAvatar>
+                    <Avatar>{username.split('')[0]}</Avatar>
+                  </ListItemAvatar>
+                  <ListItemText 
+                    primary={username}
+                    secondary={
+                      <>
+                        <Typography component="span" color="textPrimary">
+                          {chat.messages[chat.messages.length - 1].message.substring(0, 30)}
+                        </Typography>
+                      </>
+                    }
+                  >
+                  </ListItemText>
+                </ListItem>
+                <Divider/>
+              </div>
+            )
+          })
+        }
+      </List>
+    </main>
   )
 }
 
